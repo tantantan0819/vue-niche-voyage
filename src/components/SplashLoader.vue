a01a059f5795556cd9d0d06acd8992cb91805f4d
hongyu.tan <>
2025-12-28 21:41:47 +0800

    进度条：增加用户交互

--- src/components/SplashLoader.vue
+++ src/components/SplashLoader.vue
@@ -1,13 +1,20 @@
 <template>
-  <div class="SplashLoader" v-if="show">
-    <div class="progress-text">{{ Math.round(progress) }}%</div>
-    <div class="progress-wrapper">
-      <div class="progress-bar" :style="{ width: Math.max(progress, 0) + '%' }"></div>
+  <div class="SplashLoader">
+    <div class="progress-content" v-if="show" >
+      <div class="progress-text">{{ Math.round(progress) }}%</div>
+      <div class="progress-wrapper">
+        <div class="progress-bar" :style="{ width: Math.max(progress, 0) + '%' }"></div>
+      </div>
+      <div class="progress-tip">稍等片刻,旅途马上开始!</div>
     </div>
-    <div class="progress-tip">稍等片刻,旅途马上开始!</div>
+    <div class="start-journey" v-else>
+      <span @click="hideSplashLoader">开启旅程</span>
+    </div>
   </div>
 </template>
 <script setup>
+import gsap from "gsap";
+
 const props = defineProps({
   progress: {
     type: Number,
@@ -18,6 +25,9 @@ const props = defineProps({
     default: true
   }
 })
+const hideSplashLoader = () =>{
+  gsap.to('.SplashLoader',{display: 'none'})
+}
 </script>
 <style scoped>
 .SplashLoader {
@@ -37,6 +47,7 @@ const props = defineProps({
 .progress-text {
   font-size: 120px;
   color: #2c7aa5;
+  text-align: center;
 }
 
 .progress-wrapper {
@@ -62,5 +73,37 @@ const props = defineProps({
   font-size: 33px;
   color: #2c7aa5;
   margin-top: 36px;
+}
+.start-journey{
+  span{
+    font-size: 33px;
+    color: #2c7aa5;
+    position: relative;
+    cursor: pointer;
+    transition: all ease-in-out 0.5s;
+    &:hover{
+      color: rgba(44, 137, 165, 0.83);
+    }
+    &:before{
+      content: '';
+      position: absolute;
+      top: 50%;
+      left: -50px;
+      transform: translateY(-50%);
+      width: 22px;
+      height: 10px;
+      background: url('@/assets/images/menu/triangle-left.png') no-repeat center center / cover;
+    }
+    &:after{
+      content: '';
+      position: absolute;
+      top: 50%;
+      right: -50px;
+      transform: translateY(-50%);
+      width: 22px;
+      height: 10px;
+      background: url('@/assets/images/menu/triangle-right.png') no-repeat center center / cover;
+    }
+  }
 }
 </style>
